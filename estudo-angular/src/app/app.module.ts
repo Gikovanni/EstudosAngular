b0176e3dbe7a2221431fa7bc44ce73e4c01c9492
Gikovanni <you@example.com>
2024-08-26 21:56:57 -0300

    Adiciona components filtros e cards

--- estudo-angular/src/app/app.module.ts
+++ estudo-angular/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FiltersComponent } from './components/filters/filters.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FiltersComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
